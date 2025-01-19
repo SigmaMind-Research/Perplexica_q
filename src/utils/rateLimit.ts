203d7d9360427267acad2121e1cedf37d0203ae5
Gulshan-gaur <gulssh3@gmail.com>
2025-01-19 14:51:23 +0530

    improve comments

--- src/utils/rateLimit.ts
+++ src/utils/rateLimit.ts
@@ -3,7 +3,7 @@ import rateLimit from 'express-rate-limit';
 // Create a rate limiting middleware
 const rateLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000,  // 24 hours
-  max: 50,                  // Limit each IP to 100 requests per 15 minutes
+  max:100,                  // Limit each IP to 100 requests per 24 minutes
   message: 'Too many requests from this IP, please try again later.',
   headers: true,
 });
